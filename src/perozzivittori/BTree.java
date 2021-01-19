db226edf7b3295452b50a2ef122a73b169b258aa
FraVitto <francesco.vittori89@gmail.com>
2021-01-20 00:04:28 +0100

    Btree finito

--- src/perozzivittori/BTree.java
+++ src/perozzivittori/BTree.java
@@ -1,8 +1,8 @@
 package perozzivittori;
 
-public class BTree<Key extends Comparable<Key>> {
+public class BTree<K extends Comparable<K>> {
 	
-	private static final int t = 4; // t >= 2
+	private static final int t = 2; // t >= 2
 
 	private Node root;       // root of the B-tree
 	private int height;      // height of the B-tree 
@@ -12,17 +12,21 @@ public class BTree<Key extends Comparable<Key>> {
         height = 0;
     }
 	
-	public Object search(Node v, Key k, int ht) {
-		int i = 0;
-		while(i < v.m && greater(k,v.pairs[i].key)) i++;
-		if (i < v.m && eq(k,v.pairs[i].key)) return v.pairs[i].elem;
+	public Object search(K k) {
+		return search(root, k, height);
+	}
+	
+	public Object search(Node v, K k, int ht) {
+		int pos = 0;
+		while(pos < v.m && greater(k,v.pairs[pos].key)) pos++;
+		if (pos < v.m && eq(k,v.pairs[pos].key)) return v.pairs[pos].elem;
 		else {
 			if(ht == 0) return null;
-			else return search(v.children[i], k, ht-1);
+			else return search(v.children[pos], k, ht-1);
 		}
 	}
 	
-	public void insert(Key k, Object e) {
+	public void insert(K k, Object e) {
 		Node tmp = insert(root, k, e, height);
 		if (tmp == null) return;
 		// Root split
@@ -34,70 +38,69 @@ public class BTree<Key extends Comparable<Key>> {
 		newRoot.c = 2;
 		height++;
 		root = newRoot;
-		root.children[0].father = root;
-		root.children[1].father = root;
 	}
 	
-	public Node insert(Node v, Key k, Object e, int ht) {
-		int i=0;
-		while(i < v.m && greater(k,v.pairs[i].key)) i++; //research right position
+	public Node insert(Node v, K k, Object e, int ht) {
+		int pos = 0;
+		while(pos < v.m && greater(k,v.pairs[pos].key)) pos++; 				//research right position
 		InfoBT tmp = new InfoBT(k,e);
 		Node newNode = null;
+		
 		if(ht != 0)  {
-			newNode = insert(v.children[i], k, e, ht - 1); //recursive call to i-th child
-			if (newNode == null) return null;				//the insert was successful
-			tmp.key = v.children[i].pairs[t-1].key;		//t-th leaf to the father //return f.pairs[t-1] => v.children[i].pairs[t-1] 
-			tmp.elem = v.children[i].pairs[t-1].elem;   //tmp will be used to insert t-th leaf from below
-			v.children[i].pairs[t-1] = null;
+			newNode = insert(v.children[pos], k, e, ht - 1); 				//recursive call to i-th child
+			if (newNode == null) return null;								//the insert was successful
+			tmp.key = v.children[pos].pairs[t-1].key;						//t-th leaf to the father //return f.pairs[t-1] => v.children[i].pairs[t-1] 
+			tmp.elem = v.children[pos].pairs[t-1].elem;   					//tmp will be used to insert t-th leaf from below
+			v.children[pos].pairs[t-1] = null;
 		}
 		
-		if(v.m < 2*t-1) {	//if not full node
-			for (int j=v.m; j > i; j--) v.pairs[j] = v.pairs[j-1]; // slide operation for empty space for insert of the pair in i
-			v.pairs[i] = tmp;	//pair insert
-			v.m++;				//keys counter update
-			if (newNode != null) {	// if the node was split, insert of the new node as a child
-				for (int j=v.c; j > i; j--) v.children[j] = v.children[j-1]; // slide operation
-				v.children[i] = newNode;	//newNode insert
-				v.c++;			//children counter update
-			}
-		} else { //split
-			return split(v, ht);	//return newNode generated from the split
-		}
-		return null; // pair inserted 
+		//pair (and child) insertion 
+		for (int j=v.m; j > pos; j--) v.pairs[j] = v.pairs[j-1]; 			//slide operation for empty space for insert of the pair in i
+		v.pairs[pos] = tmp;													//pair insert
+		v.m++;																//keys counter update
+		if (newNode != null) {												//if the node was split, insert of the new node as a child
+			for (int j=v.c; j > pos; j--) v.children[j] = v.children[j-1];	//slide operation
+			v.children[pos] = newNode;										//newNode insert
+			v.c++;															//children counter update
+		} 
+		
+		if(v.m > 2*t-1) return split(v, ht);								//return newNode generated from the split
+		return null; 														//pair inserted			 
 	}
 	
 	private Node split(Node f, int ht) {
 		Node tmp = new Node();
-		tmp.father = f.father;	//same father
-		f.m = t-1;				//f left node
-		tmp.m = t;				//tmp right node
-		if (ht != 0) {			//children division (DA RIVEDERE)
-			tmp.c = f.c/2; 
-			f.c = f.c/2 - 1;		
+		f.m = t-1;	//f   left node
+		tmp.m = t;	//tmp right node
+		
+		//pairs split
+		for (int j=0; j < tmp.m; j++) tmp.pairs[j] = f.pairs[t+j];
+		
+		//children split
+		if (ht != 0) {
+			tmp.c = t+1; 
+			f.c = t;
+			for (int j=0; j < tmp.c; j++) tmp.children[j] = f.children[t+j];
 		}
-		for (int j=0; j < tmp.m; j++) {		//split operation (DA RIVEDERE)
-			tmp.pairs[j] = f.pairs[t+j];	
-			if (ht != 0) tmp.children[j] = f.children[t+j];
-		}
-		//for (int j=0; j < t; j++) f.pairs[j] = f.pairs[t+j];	
-		return tmp;		//return new node
+		
+		return tmp;	//return new node
 	}
 	
-    public void delete(Key k) {
+    public void delete(K k) {
     	delete(null, 0, root, k, height);
     }
     
-	public boolean delete(Node father, int childInd, Node v, Key k, int ht) {
-		int i=0;
-		while(i < v.m && greater(k,v.pairs[i].key)) i++; //research right position
-		if(i < v.m && eq(k,v.pairs[i].key)) {
+	public boolean delete(Node father, int childInd, Node v, K k, int ht) {
+		int pos = 0;
+		while(pos < v.m && greater(k,v.pairs[pos].key)) pos++; //research right position
+		if(pos < v.m && eq(k,v.pairs[pos].key)) {
 			if (ht != 0) { // intern node 
-				v.pairs[i] = extractGreatest(v, i, v.children[i], ht); //return v predecessor
+				v.pairs[pos] = extractGreatest(v, pos, v.children[pos], ht); //return v predecessor
 			} else { //leaf
-				deleteFromLeaf(father, childInd, v, i);
+				deleteFromLeaf(father, childInd, v, pos);
 			}
 		} else {
-			if (ht != 0) return delete(v, i, v.children[i], k, ht - 1);
+			if (ht != 0) return delete(v, pos, v.children[pos], k, ht - 1);
 			else return false;
 		}
 		return true;
@@ -112,15 +115,20 @@ public class BTree<Key extends Comparable<Key>> {
 	
 	private void deleteFromLeaf(Node father, int childInd, Node leaf, int d) {
 		leaf.pairs[d] = null;
-		if (leaf.m > t-1) {													//normal delete	
+		//normal delete
+		if (leaf.m > t-1 || father == null) {														
 			leaf.m--;														//pairs counter update
 			for(int i=d; i < leaf.m; i++) leaf.pairs[i] = leaf.pairs[i+1];	//slide operation 
-		} else {
+		} else {											//avoiding NullPointerException
 			Node leftSibling  = null;
 			Node rightSibling = null;
 			if(childInd != 0)			 leftSibling  = father.children[childInd-1];
 			if(childInd != father.c - 1) rightSibling = father.children[childInd+1];
+			
+			//redistribution
 			boolean redistribution = false;
+			
+			//left redistribution
 			if (leftSibling != null && leftSibling.m > t-1 ) {							//redistribution from left sibling
 				for(int i=0; i < d; i++) leaf.pairs[i+1] = leaf.pairs[i];				//slide
 				leaf.pairs[0] = father.pairs[childInd-1];								//pairs swap
@@ -128,7 +136,9 @@ public class BTree<Key extends Comparable<Key>> {
 				leftSibling.pairs[leftSibling.m-1] = null;
 				leftSibling.m--;
 				redistribution = true;
-			} 
+			}
+			
+			//right redistribution
 			if (rightSibling != null && rightSibling.m > t-1 ) {						//redistribution from right sibling
 				for(int i=leaf.m-1; i > d; i++) leaf.pairs[i-1] = leaf.pairs[i];		//slide
 				leaf.pairs[leaf.m-1] = father.pairs[childInd];							//pairs swap
@@ -138,60 +148,59 @@ public class BTree<Key extends Comparable<Key>> {
 				redistribution = true;					
 			}
 			if (redistribution) return;
+			
 			// fusion
-			if (leftSibling != null) {																			//left fusion
-				leftSibling.pairs[leftSibling.m] = father.pairs[childInd]; leftSibling.m++; 					//pair from father
+			// left fusion
+			if (leftSibling != null) {																			
+				leftSibling.pairs[leftSibling.m] = father.pairs[childInd]; leftSibling.m++;	//pair from father
 				father.pairs[childInd] = null;
 				int k = 0;			
 				for(int i=0; i < leaf.m; i++) {
-					if(i==d) k++;											//double index to avoid making another slide operation on leaf pairs
-					leftSibling.pairs[i+leftSibling.m] = leaf.pairs[k];		//pairs transfer to left sibling
+					if(i==d) k++;															//double index to avoid making another slide operation on leaf pairs
+					leftSibling.pairs[i+leftSibling.m] = leaf.pairs[k];						//pairs transfer to left sibling
 					k++; 
 				}
 				leftSibling.m += leaf.m - 1;																			
-				for(int i=childInd; i < father.m; i++) {														//slide operation on the father
-					father.pairs[i] = father.pairs[i+1];
-					father.children[i] = father.children[i+1];
-				}
+				for(int i=childInd-1; i < father.m; i++) father.pairs[i] = father.pairs[i+1];
+				for(int i=childInd-1; i < father.c; i++) father.children[i] = father.children[i+1];
 				father.m--; father.c--;
+				
+			//right fusion	
 			} else if (rightSibling != null){
-				for(int i=0; i < leaf.m-1; i++) rightSibling.pairs[i+leaf.m] = rightSibling.pairs[i];			//slide operation on sibling (right)
+				for(int i=0; i < leaf.m-1; i++) rightSibling.pairs[i+leaf.m] = rightSibling.pairs[i];	//slide operation on sibling (right)
 				rightSibling.pairs[leaf.m-1] = father.pairs[childInd-1]; rightSibling.m++;
 				father.pairs[childInd-1] = null;
 				int k = 0;
 				for(int i=0; i < leaf.m; i++) {
-					if(i==d) k++;											//double index to avoid making another slide operation on leaf pairs
-					rightSibling.pairs[i] = leaf.pairs[k];					//pairs transfer to right sibling
+					if(i==d) k++;															
+					rightSibling.pairs[i] = leaf.pairs[k];												//pairs transfer to right sibling
 					k++;
 				}
 				rightSibling.m += leaf.m - 1;
-				for(int i=childInd-1; i < father.m; i++) {
-					father.pairs[i] = father.pairs[i+1];
-					father.children[i] = father.children[i+1];
-				}
+				for(int i=childInd-1; i < father.m; i++) father.pairs[i] = father.pairs[i+1];
+				for(int i=childInd-1; i < father.c; i++) father.children[i] = father.children[i+1];
 				father.m--; father.c--;
 			}
 		}
 	}
 	
-	// comparison functions - make Comparable instead of Key to avoid casts
+	// comparison functions - make Comparable instead of K to avoid casts
     private boolean greater(Comparable k1, Comparable k2) {
         return k1.compareTo(k2) > 0;
     }
 
     private boolean eq(Comparable k1, Comparable k2) {
         return k1.compareTo(k2) == 0;
-    }
+    }    
     	
 	protected static class Node {
-		protected Node father;
 		protected int m;                             			// number of pairs
 		protected int c;										// number of children
-		protected InfoBT[] pairs = new InfoBT[2*t-1];			// array of key-value pairs
+		protected InfoBT[] pairs = new InfoBT[2*t];				// array of key-value pairs (plus one for operations)
 		protected Node[] children = new Node[2*t];   			// array of children
 
         // create a node
-		protected Node() {m=0; father=null;}
+		protected Node() {m=0;}
     }
 	
 	protected static class InfoBT{
