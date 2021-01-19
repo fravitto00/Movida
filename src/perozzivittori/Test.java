d38dfff512205e29a21f663a51500a81f17b8319
Narto <marcoperozzi00@gmail.com>
2021-01-19 16:57:40 +0100

    ArrayOrdinato done

--- src/perozzivittori/Test.java
+++ src/perozzivittori/Test.java
@@ -6,7 +6,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		MovidaCore bona = new MovidaCore();
-		File f = new File("C:\\Users\\Utente\\eclipse-workspace\\movida\\commons\\esempio-formato-dati.txt");
+		File f = new File("C:\\Users\\UtenteHP\\Documents\\movida\\Movida\\src\\movida\\commons\\esempio-formato-dati.txt");
+		//"C:\\Users\\Utente\\eclipse-workspace\\Movida\\src\\movida\\commons\\esempio-formato-dati.txt"
 		bona.loadFromFile(f);
 	}
 }
