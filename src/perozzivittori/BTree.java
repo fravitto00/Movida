5482b932bdb68311c57e956d18f990d66f9f5219
FraVitto <francesco.vittori89@gmail.com>
2021-02-06 01:01:08 +0100

    Beautify BTree

--- src/perozzivittori/BTree.java
+++ src/perozzivittori/BTree.java
@@ -12,39 +12,87 @@ public class BTree implements Dizionario {
 	 */
 	private static final int t = 2; // Grado BTree t >= 2
 
-	private Node root;		// Radice
-	private int height;		// Altezza 	
+	private Node root;				// Radice
+	private int height;				// Altezza 	
+	
+	/**
+     * Classe Nodo
+     */
+	private class Node {
+		int m;                             	// Numero di coppie del nodo
+		int c;								// Numero di figli del nodo
+		InfoBT[] pairs = new InfoBT[2*t];	// Coppie Chiave-Elemento (maggiorate di una per le operazioni di split)
+		Node[] children = new Node[2*t+1];	// Figli del nodo (maggiorati di uno per le operazioni di split)
+
+		private Node() {m=0;}
+    }
+	
+	/**
+     * Classe Coppia Chiave-Elemento
+     */
+	private class InfoBT{
+		Object elem;
+		String key;
+
+		private InfoBT(String k, Object e){
+			key = k;
+			elem = e;
+		}
+	}
 	
 	/**
 	 * Costruttore
 	 */
 	public BTree() {
-        	root = new Node();
-        	height = 0;
-    	}
+		root = new Node();
+		height = 0;
+    }
 	
 	/**
-	 * Metodo pubblico per l'operazione di Search
+	 * Metodo pubblico per la ricerca del record
+	 * 
+	 * @param  k titolo/nome da ricercare
+	 * 
+	 * @return record con chiave k se esistente, null altrimenti
 	 */
-	@Override
 	public Object search(String k) {
-		return search(root, k, height);
+		if (k != null) return search(root, k, height);
+		return null;
 	}
 	
+	/**
+	 * Ricerca della chiave tramite ricorsione	
+	 * 
+	 * @param v il nodo attuale
+	 * @param k la chiave da ricercare
+	 * @param ht l'altezza attuale
+	 * 
+	 * @return record con chiave k se esistente, null altrimenti
+	 */
 	public Object search(Node v, String k, int ht) {
 		int pos = 0;
+		
+		// Ricerca posizione
 		while(pos < v.m && k.compareTo(v.pairs[pos].key) > 0) pos++;
+		
 		if (pos < v.m && k.equals(v.pairs[pos].key)) return v.pairs[pos].elem;
 		else {
 			if(ht == 0) return null;
 			else return search(v.children[pos], k, ht-1);
 		}
 	}
-	
+
+	/**
+	 * Metodo pubblico per l'inserimento del record
+	 * 
+	 * @param k il titolo/nome 
+	 * @param e il record
+	 */
 	public void insert(String k, Object e) {
 		Node tmp = insert(root, k, e, height);
 		if (tmp == null) return;
-		// Root split
+		
+		// Split sulla Radice
 		Node newRoot = new Node();
 		newRoot.pairs[0] = root.pairs[t-1];
 		newRoot.children[0] = root;
@@ -53,154 +101,229 @@ public class BTree implements Dizionario {
 		newRoot.c = 2;
 		height++;
 		root = newRoot;
-		//System.out.println("New Root! Ht: " + height);
 	}
 	
+	/**
+	 * Inserimento del record tramite ricorsione
+	 * 
+	 * @param v il nodo attuale
+	 * @param k la chiave da inserire
+	 * @param e il record da inserire
+	 * @param ht l'altezza attuale
+	 * 
+	 * @return il nuovo nodo risultante dall'eventuale split (numero eccessivo di record nel nodo)
+	 */
 	public Node insert(Node v, String k, Object e, int ht) {
 		int pos = 0;
-		while(pos < v.m && k.compareTo(v.pairs[pos].key) > 0) pos++; 				//research right position
+		
+		// Ricerca posizione
+		while(pos < v.m && k.compareTo(v.pairs[pos].key) > 0) pos++;
+		
 		InfoBT tmp = new InfoBT(k,e);
 		Node newNode = null;
 		
 		if(ht != 0)  {
-			newNode = insert(v.children[pos], k, e, ht - 1); 				//recursive call to i-th child | returns new split node
-			if (newNode == null) return null;								//the insert was successful
-			tmp.key = v.children[pos].pairs[t-1].key;						//t-th leaf to the father //return f.pairs[t-1] => v.children[i].pairs[t-1] 
-			tmp.elem = v.children[pos].pairs[t-1].elem;   					//tmp will be used to insert t-th leaf from below
+			newNode = insert(v.children[pos], k, e, ht - 1); 				// Chiamata ricorsiva al figlio i | ritorna il nuovo nodo risultante dallo split
+			if (newNode == null) return null;								// L'inserimento è riuscito
+			
+			// La coppia t da spostare nel padre
+			tmp.key = v.children[pos].pairs[t-1].key;						  
+			tmp.elem = v.children[pos].pairs[t-1].elem;   					
 			v.children[pos].pairs[t-1] = null;
 		}
 		
-		//pair (and child) insertion 
-		for (int j=v.m; j > pos; j--) v.pairs[j] = v.pairs[j-1]; 			//slide operation for empty space for insert of the pair in i
-		v.pairs[pos] = tmp;													//pair insert
-		v.m++;																//keys counter update
-		if (newNode != null) {												//if the node was split, insert of the new node as a child
-			for (int j=v.c; j > pos+1; j--) v.children[j] = v.children[j-1];	//slide operation
-			v.children[pos+1] = newNode;										//newNode insert
-			v.c++;															//children counter update
+		// Inserimento della coppia
+		for (int j=v.m; j > pos; j--) v.pairs[j] = v.pairs[j-1]; 			 // Operazione di slide
+		v.pairs[pos] = tmp;
+		v.m++;
+		
+		// Inserimento del nuovo nodo come figlio, in caso di split
+		if (newNode != null) {												
+			for (int j=v.c; j > pos+1; j--) v.children[j] = v.children[j-1]; // Operazione di slide
+			v.children[pos+1] = newNode;
+			v.c++;
 		} 
 		
-		if(v.m > 2*t-1) return split(v, ht);								//return newNode generated from the split
-		return null; 														//pair inserted			 
+		// Se il nodo attuale ha più coppie del numero consentito, split
+		if(v.m > 2*t-1) return split(v, ht);								// Ritorno del nuovo nodo generato
+		return null; 														// L'inserimento è riuscito
 	}
 	
+	/**
+	 * Operazione di split del nodo
+	 * 
+	 * @param f il nodo sui cui effettuare lo split
+	 * @param ht l'altezza del nodo f
+	 * 
+	 * @return il nuovo nodo risultante dallo split
+	 */
 	private Node split(Node f, int ht) {
 		Node tmp = new Node();
-		f.m = t-1;	//f   left node
-		tmp.m = t;	//tmp right node
+		f.m = t-1;	// f   nodo di sinistra
+		tmp.m = t;	// tmp nodo di destra
 		
-		//pairs split
+		// Redistribuzione delle coppie
 		for (int j=0; j < tmp.m; j++) tmp.pairs[j] = f.pairs[t+j];
 		
-		//children split
+		// Se il nodo non è una foglia viene effettuata la redistribuzione dei figli 
 		if (ht != 0) {
 			tmp.c = t+1; 
 			f.c = t;
 			for (int j=0; j < tmp.c; j++) tmp.children[j] = f.children[t+j];
 		}
 		
-		return tmp;	//return new node
+		return tmp;
 	}
 	
+	/**
+	 * Metodo pubblico per la cancellazione di un record
+	 * 
+	 * @param k la chiave da eliminare
+	 * 
+	 * @return il record eliminato se esistente, null altrimenti
+	 */
     public Object delete(String k) {
     	return delete(null, 0, root, k, height);
     }
     
+    /**
+     * Cancellazione del record tramite ricorsione
+     * 
+     * @param father il nodo padre del nodo attuale
+     * @param childInd l'indice del nodo attuale tra i figli del padre
+     * @param v il nodo attuale
+     * @param k la chiave da eliminare
+     * @param ht l'altezza attuale
+     * 
+     * @return il record eliminato se esistente, null altrimenti
+     */
 	public Object delete(Node father, int childInd, Node v, String k, int ht) {
 		int pos = 0;
 		Object deletedObj = null;
-		while(pos < v.m && k.compareTo(v.pairs[pos].key) > 0) pos++; //research right position
+		
+		// Ricerca posizione
+		while(pos < v.m && k.compareTo(v.pairs[pos].key) > 0) pos++;
+		
 		if(pos < v.m && k.equals(v.pairs[pos].key)) {
-			if (ht != 0) { // intern node 
-				deletedObj = v.pairs[pos].elem;
-				v.pairs[pos] = extractGreatest(v, pos, v.children[pos], ht - 1); //return v predecessor
+			if (ht != 0) { 														 // Nodo Interno
+				deletedObj = v.pairs[pos].elem;									 // Il record eliminato, per il return
+				v.pairs[pos] = extractGreatest(v, pos, v.children[pos], ht - 1); // Ritorna il predecessore di v
 				delete(v, pos, v.children[pos], v.pairs[pos].key, ht - 1);
-			} else { //leaf
+			} else { 															 // Foglia
 				deletedObj  = deleteFromLeaf(father, childInd, v, pos);
 			}
 		}
 		
-		if (ht != 0 && deletedObj==null) deletedObj = delete(v, pos, v.children[pos], k, ht - 1);
+		// Chiamata ricorsiva
+		if (ht != 0 && deletedObj==null) 
+			deletedObj = delete(v, pos, v.children[pos], k, ht - 1);
 		
-		//Ribilanciamento
-		if(v.m < t-1 && father != null) {
-			//System.out.println("Nodo interno invalido");
+		// Se il nodo attuale ha più coppie del numero consentito, ribilanciamento
+		if(v.m < t-1 && father != null)
 			balance(father, childInd, v, t-2);
-		}
 		
 		return deletedObj;
 	}
 	
+	/**
+	 * Estrazione del predecessore
+	 * 
+	 * @param father il nodo padre del nodo attuale
+	 * @param childInd l'indice del nodo attuale tra i figli del padre
+	 * @param v il nodo attuale
+	 * @param ht l'altezza attuale
+	 * 
+	 * @return il predecessore
+	 */
 	private InfoBT extractGreatest(Node father, int childInd, Node v, int ht) {
-		
-		if (ht != 0) return extractGreatest(v, v.c-1, v.children[v.c-1], ht - 1);		//v.c-1 equals to the last children (the greatest)
-		//System.out.println(v.pairs[v.m-1].key);
-		return v.pairs[v.m-1];															//v.m-1 equals to the last pair (the greatest)
+		if (ht != 0) return extractGreatest(v, v.c-1, v.children[v.c-1], ht - 1);		//v.c-1 equivale all'ultimo figlio (contiene la chiave più grande)
+		return v.pairs[v.m-1];															//v.m-1 equivale all'ultima coppia (la chiave più grande)
 	}
 	
+	/**
+	 * Cancellazione da foglia
+	 * 
+	 * @param father il nodo padre del nodo attuale
+	 * @param childInd l'indice del nodo attuale tra i figli del padre
+	 * @param leaf il nodo foglia
+	 * @param d l'indice della coppia da eliminare
+	 * 
+	 * @return il record eliminato
+	 */
 	private Object deleteFromLeaf(Node father, int childInd, Node leaf, int d) {
 		Object deletedObj = leaf.pairs[d].elem;
 		leaf.pairs[d] = null;
-		if (leaf.m > t-1 || father == null) {														
-			leaf.m--;														//pairs counter update
-			for(int i=d; i < leaf.m; i++) leaf.pairs[i] = leaf.pairs[i+1];	//slide operation 
-			//printTree();
+		
+		if (leaf.m > t-1 || father == null) {								// Se l'eliminazione non compromette la foglia
+			leaf.m--;														
+			for(int i=d; i < leaf.m; i++) leaf.pairs[i] = leaf.pairs[i+1];	// Operazione di slide
 		}
-		else balance(father, childInd, leaf, d);
+		else balance(father, childInd, leaf, d);							// Altrimenti ribilanciamento
 		return deletedObj;
 	}
 	
+	/**
+	 * Ribilanciamento del BTree
+	 * 
+	 * @param father il nodo padre del nodo attuale
+	 * @param childInd l'indice del nodo attuale tra i figli del padre
+	 * @param v il nodo attuale da bilanciare
+	 * @param d l'indice della coppia eliminata
+	 */
 	private void balance(Node father, int childInd, Node v, int d) {
 		Node leftSibling  = null;
 		Node rightSibling = null;
+		
+		// Acquisizione fratelli
 		if(childInd > 0)			leftSibling  = father.children[childInd-1];
 		if(childInd < father.c - 1)	rightSibling = father.children[childInd+1];
 			
-		//REDISTRIBUTION
+		// REDISTRIBUZIONE
 		boolean redistribution = false;
 			
-		//left redistribution
-		if (leftSibling != null && leftSibling.m > t-1 ) {							//redistribution from left sibling
-			for(int i=0; i < d; i++) v.pairs[i+1] = v.pairs[i];						//slide
-			v.pairs[0] = father.pairs[childInd-1];								//pairs swap with left sibling
-			father.pairs[childInd-1] = leftSibling.pairs[leftSibling.m-1];			//pairs swap
+		// Redistribuzione da sinistra
+		if (leftSibling != null && leftSibling.m > t-1 ) {							
+			for(int i=0; i < d; i++) v.pairs[i+1] = v.pairs[i];				// Operazione di slide
+			v.pairs[0] = father.pairs[childInd-1];							// Coppia dal padre
+			father.pairs[childInd-1] = leftSibling.pairs[leftSibling.m-1];	// Il padre riceve la coppia dal fratello di sinistra
 			leftSibling.pairs[leftSibling.m-1] = null;
 			leftSibling.m--;
-			if(v.c > 0) {															//internal node (with children)
-				for(int i=0; i < v.c; i++) v.children[i+1] = v.children[i];			//slide children
+			
+			// Nodo interno (se ha figli)
+			if(v.c > 0) {
+				for(int i=0; i < v.c; i++) v.children[i+1] = v.children[i];	// Operazione di slide sui figli
 				v.c++;
 				leftSibling.c--;
-				v.children[0] = leftSibling.children[v.c];
+				v.children[0] = leftSibling.children[v.c];					// Il nodo v riceve il figlio dal fratello di sinistra
 				leftSibling.children[v.c] = null;
 			}
 			redistribution = true;
 		}
 			
-		//right redistribution
+		// Redistribuzione da destra
 		if (rightSibling != null && rightSibling.m > t-1 ) {						//redistribution from right sibling
-			for(int i=v.m-1; i > d; i--) v.pairs[i-1] = v.pairs[i];					//slide if d isn't last pair
+			for(int i=v.m-1; i > d; i--) v.pairs[i-1] = v.pairs[i];					// Operazione di slide se d non è l'ultima coppia (v.m-1)
 			if (v.m == 0) v.m++;														//pairs swap with right sibling
 			v.pairs[v.m-1] = father.pairs[childInd];								// v is invalid in case of t=2
 			father.pairs[childInd] = rightSibling.pairs[0];							//pairs swap
 			rightSibling.m--;
-			for(int i=0; i < rightSibling.m; i++) rightSibling.pairs[i] = rightSibling.pairs[i+1];	//slide operation for sibling
-			if(v.c > 0) {																			//internal node (with children)
-				v.children[v.c] = rightSibling.children[0]; 													//sottoalbero passing with rotation
+			for(int i=0; i < rightSibling.m; i++) rightSibling.pairs[i] = rightSibling.pairs[i+1];			 //slide operation for sibling
+			if(v.c > 0) {																					 //internal node (with children)
+				v.children[v.c] = rightSibling.children[0]; 												 //sottoalbero passing with rotation
 				v.c++;	
 				rightSibling.c--;
-				for(int i=0; i < rightSibling.c; i++) rightSibling.children[i] = rightSibling.children[i+1];	//slide children
+				for(int i=0; i < rightSibling.c; i++) rightSibling.children[i] = rightSibling.children[i+1]; //slide children
 			}
 			redistribution = true;
 		}
-		if (redistribution) {
-			printTree(root, height);
-			return;
-		}
 		
-		//FUSION
+		if (redistribution)
+			return;
+		
+		// FUSIONE
 		boolean fusion = false;
 		
-		//left fusion
+		// Fusione a sinistra
 		if (leftSibling != null) {																			
 			leftSibling.pairs[leftSibling.m] = father.pairs[childInd-1];	//pair from father
 			leftSibling.m++;
@@ -226,7 +349,7 @@ public class BTree implements Dizionario {
 			}
 			fusion = true;
 				
-		//right fusion	
+		// Fusione a destra	
 		} else if (rightSibling != null){
 			if (v.m == 0) v.m++;																//internal node with 0 (t-2) keys
 			for(int i=0; i < v.m; i++) rightSibling.pairs[i+v.m] = rightSibling.pairs[i];	//slide operation on sibling (right)
@@ -265,6 +388,11 @@ public class BTree implements Dizionario {
 		}
 	}
     
+	/**
+	 * Metodo pubblico per la creazione di un array contenente i record del BTree
+	 * 
+	 * @return l'array di record
+	 */
     public Object[] toArray() {
     	List<Object> ll = new LinkedList<>();
     	buildLL(ll, root);
@@ -272,6 +400,12 @@ public class BTree implements Dizionario {
     	else return null;
     }
     
+    /**
+     * Creazione di una lista contenente i record del BTree tramite ricorsione
+     * 
+     * @param ll la lista
+     * @param v il nodo attuale
+     */
     private void buildLL(List<Object> ll, Node v) {
     	if (v != null) {
     		int i;
@@ -280,11 +414,15 @@ public class BTree implements Dizionario {
     			ll.add(v.pairs[i].elem);
     		}
     		
-    		//last child
+    		// L'ultimo figlio (nFigli = nCoppie + 1)
     		if(v.c > i) buildLL(ll, v.children[i]);
     	}
     }
     
+    /**
+     * Stampa ricorsiva del BTree per debug
+     */
+    /*
     public void printTree() {
     	printTree(root, height);
     }
@@ -303,24 +441,5 @@ public class BTree implements Dizionario {
     		if(v.c > i) printTree(v.children[i], ht - 1);
     	}
     }
-   
-	private class Node {
-		int m;                             			// number of pairs
-		int c;										// number of children
-		InfoBT[] pairs = new InfoBT[2*t];				// array of key-value pairs (plus one for operations)
-		Node[] children = new Node[2*t+1];   			// array of children
-
-        // create a node
-		private Node() {m=0;}
-    }
-	
-	private class InfoBT{
-		Object elem;
-		String key;
-
-		private InfoBT(String k, Object e){
-			key = k;
-			elem = e;
-		}
-	}
+    */
 }
